81d256a2820c16cca8f14d292e82a71836cda89d
komachi16 <haru16.nhs@gmail.com>
2024-09-12 13:28:05 +0900

    init project

---
+++ SwiftTechNote/SwiftTechNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTechNote
+//
+//  Created by komachi16 on 2024/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTechNote/SwiftTechNote/SwiftTechNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTechNoteApp.swift
+//  SwiftTechNote
+//
+//  Created by komachi16 on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTechNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
